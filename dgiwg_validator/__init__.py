2cdfacf1eafd20cb3f23af1c28d0a4ae15bfac08
Xaxis2y <143286427+Xaxis2y@users.noreply.github.com>
2026-06-15 12:54:01 -0400

    Delete dgiwg_validator directory

--- dgiwg_validator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""DGIWG GeoPackage Compliance Validator v1.55.
-
-Usage (package):   python -m dgiwg_validator [FILE_OR_FOLDER ...]
-Usage (zipapp):    python DGIWG_Validator_v1_54.pyz [FILE_OR_FOLDER ...]
-"""
-__version__ = "1.55"
